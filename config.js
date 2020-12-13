5a7b53457e16061d6539c8fb338044914b028d88
Darek Kay <hello@darekkay.com>
2020-12-13 15:36:54 +0100

    Add Nunito web font

--- config.js
+++ config.js
@@ -4,6 +4,11 @@ const path = (...arguments) => join(__dirname, ...arguments);
 
 module.exports = {
   paths: {
+    assets: {
+      source: path("src", "fonts", "nunito"),
+      destination: path("dist", "css", "fonts"),
+    },
+
     destination: path("dist"),
 
     content: path("public") + "/**/*.html",
--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ const { gulp, tasks } = require("@darekkay/gulp");
 
 const config = require("./config");
 const { series, parallel } = gulp;
-const { clean, styles, content, env } = tasks(config);
+const { clean, styles, content, assets, env } = tasks(config);
 
-const build = series(env("production"), styles, content);
+const build = series(env("production"), styles, assets, content);
 
 module.exports = { clean, build };
