178f7b7dd435efce953ebd98e79d331a7efedb25
Darek Kay <hello@darekkay.com>
2020-12-22 14:21:49 +0100

    Add Prism.js styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,42 @@
+const { join } = require("path");
 const { gulp, tasks } = require("@darekkay/gulp");
 
-const config = require("./config");
-const { series, parallel } = gulp;
+const path = (...arguments) => join(__dirname, ...arguments);
+
+const config = {
+  paths: {
+    assets: {
+      source: path("src", "fonts", "nunito"),
+      destination: path("dist", "css", "fonts"),
+    },
+
+    destination: path("dist"),
+
+    content: path("public") + "/**/*.html",
+
+    styles: [
+      {
+        source: path("src", "_all.scss"),
+        destination: path("dist", "css"),
+        watch: path("src"),
+      },
+      {
+        source: path("src", "vendor", "highlightjs", "_all.scss"),
+        destination: path("dist", "css"),
+        fileName: "highlightjs.css",
+        watch: path("src"),
+      },
+      {
+        source: path("src", "vendor", "prismjs", "_all.scss"),
+        destination: path("dist", "css"),
+        fileName: "prismjs.css",
+        watch: path("src"),
+      },
+    ],
+  },
+};
+
+const { series } = gulp;
 const { clean, styles, content, assets, env } = tasks(config);
 
 const build = series(env("production"), styles, assets, content);
